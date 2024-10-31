994aa576b7892c774bfeab8cee329c9746b26831
Arthur Peruzzo <peruzzoarthur@gmail.com>
2024-10-31 12:33:02 -0300

    added more colors for randomizing first geojson color

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,14 +7,25 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+enum Color {
+  Purple = "#370665",
+  Blue = "#35589A",
+  Orange = "#F14A16",
+  Yellow = "#FC9918",
+  Green = "#7ED4AD",
+  Pink = "#D76C82",
+  Red = "#F95454",
+  Cyan = "#77CDFF",
+  Emerald = "#41B3A2",
+}
 export const getRandomColor = (): string => {
-  const colors = ["#370665", "#35589A", "#F14A16", "#FC9918"];
+  const colors = Object.values(Color);
   const randomIndex = Math.floor(Math.random() * colors.length);
   return colors[randomIndex];
 };
 
 export const getBoundingBoxCenter = (
-  bbox: BBox | undefined
+  bbox: BBox | undefined,
 ): LatLngTuple | undefined => {
   if (!bbox) {
     return;
